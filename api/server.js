f35f5fa3b9b672a81c6f9c95a34affa2242f23f2
Cem <cemkgrbz@gmail.com>
2023-01-12 14:03:05 +0100

    added client and api folders

---
+++ api/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors())
+
+//database connection
+
+mongoose.set('strictQuery',false);
+mongoose.connect("mongodb://127.0.0.1:27017/todo-mern", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log("Connected to DB"))
+    .catch(console.error);
+
+
+//routes and controllers
+
+const Todo = require('./models/Todo');
+
+app.get('/todos', async (req, res) => {
+	const todos = await Todo.find();
+
+	res.json(todos);
+});
+
+app.post('/todo/new', (req, res) => {
+	const todo = new Todo({
+		text: req.body.text
+	})
+
+	todo.save();
+
+	res.json(todo);
+});
+
+
+app.listen(3001, () => console.log("Server started on port 3001"));
